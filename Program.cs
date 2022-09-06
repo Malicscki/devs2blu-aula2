70e01695013f8dc3c041f0db46d1a1e2fb2c0cc6
Malicscki <106198134+Malicscki@users.noreply.github.com>
2022-09-06 20:44:55 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaraçao de variaveis
+            string formatacaoDados = "Inicialização de Variável...\n"; 
+            string nomeUsuario, cidadeUsuario;
+
+            
+            // Entrada de Dados
+            Console.WriteLine("Informe seu nome: ");
+            nomeUsuario = Console.ReadLine();
+
+            // Processamento
+            formatacaoDados += "Seja bem vindo, " + nomeUsuario + "!";
+
+            // Apresentaçao dos dados
+            Console.WriteLine(formatacaoDados);
+           
+            // Pausa antes de encerrar aplicaçao 
+            Console.WriteLine("Pressione qualquer tecla para sair...");
+            var input = Console.ReadLine();
+        }
+    }
+}
